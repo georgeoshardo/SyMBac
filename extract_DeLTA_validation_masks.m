444471bb9a8fb34c44dbd6e8b1cbdf1403783fa1
Georgeos Hardo <georgeoshardo@protonmail.com>
2021-06-15 15:20:11 +0100

    Test against DeLTA validation data
    Former-commit-id: 04740b60620c33ed402ddc27766e17c689b1659a [formerly 68b0e1eece12a327a241facf0249a018dbf978ed]
    Former-commit-id: 634122d59b1a2e90563e7faa60bbce5b1b2ae1c4

---
+++ extract_DeLTA_validation_masks.m
@@ -0,0 +1,18 @@
+DeLTA_mat_file_dir = "/home/georgeos/Storage/Dropbox (Cambridge University)/PhD_Georgeos_Hardo/ML_based_segmentation_results/DeLTA_data/mother_machine/evaluation/postprocessed/"
+mat_file_names = dir(strcat(DeLTA_mat_file_dir,"*.mat"));
+output_dir = strcat(DeLTA_mat_file_dir,"masks/")
+f = waitbar(0, 'Starting');
+for x=1:length(mat_file_names)
+    current_dir = strcat(mat_file_names(1).folder,"/",mat_file_names(1).name);
+    data = load(current_dir);
+    chambers = length(data.res);
+    for y=1:chambers
+        timepoints = table(size(data.res(y).labelsstack)).Var1(3);
+        for z=1:timepoints
+        current_image_name = strcat("Position",num2str(x,"%02.f"),"_Chamber",num2str(y,"%02.f"),"_Frame",num2str(z,"%03.f"),".png");
+        imwrite(data.res(y).labelsstack(:,:,z),strcat(output_dir,current_image_name))
+        end
+    end
+    waitbar(x/length(mat_file_names), f, sprintf('Progress: %d %%', floor(x/length(mat_file_names)*100)))
+end
+
