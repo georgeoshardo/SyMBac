2d831269e292abc0a09c755106f8e51799365de1
Georgeos Hardo <georgeoshardo@protonmail.com>
2021-10-25 17:28:26 +0100

    Update ver no

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 
 setup(
     name='SyMBac',
-    version='0.1.3',    
+    version='0.1.4',    
     description='A package for generating synthetic images of bactera in phase contrast or fluorescence. Used for creating training data for machine learning segmentation and tracking algorithms.',
     url='https://github.com/georgeoshardo/SyMBac',
     author='Georgeos Hardo',
