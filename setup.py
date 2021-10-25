74783feccb1865c2d401ad8e235b865b1e44f9a6
Georgeos Hardo <georgeoshardo@protonmail.com>
2021-10-25 17:22:50 +0100

    remove dependencies

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
 
 setup(
     name='SyMBac',
-    version='0.1.0',    
+    version='0.1.3',    
     description='A package for generating synthetic images of bactera in phase contrast or fluorescence. Used for creating training data for machine learning segmentation and tracking algorithms.',
     url='https://github.com/georgeoshardo/SyMBac',
     author='Georgeos Hardo',
     author_email='gh464@cam.ac.uk',
     license='GPL-2.0',
     packages=['SyMBac','SyMBac.external','SyMBac.sample_images','SyMBac.external.DeLTA'],
-    long_description = "SyMBac is a tool to generate synthetic phase contrast or fluorescence images of bacteria. Currently the tool only supports bacteria growing in the mother machine, however support for bacteria growing in monolayers (and maybe even biofilms!) is coming.",
-
+    long_description = (this_directory / "README.md").read_text(), #"SyMBac is a tool to generate synthetic phase contrast or fluorescence images of bacteria. Currently the tool only supports bacteria growing in the mother machine, however support for bacteria growing in monolayers (and maybe even biofilms!) is coming.",
+    long_description_content_type='text/markdown',
     install_requires=['tensorflow==2.6.0',
                       'elasticdeform==0.4.9', 
                       'tifffile==2021.10.12',
                       'scikit-image==0.18.3' ,
                       'matplotlib==3.4.3',
-                      "csbdeep==0.6.3",
-                      "stardist==0.7.3",
                       "tqdm==4.62.3",
                       "pandas==1.3.4",
                       "natsort==7.1.1",
