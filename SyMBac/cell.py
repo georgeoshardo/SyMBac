2e25b84f2874fbcdf70fe34bdfbafcc8f7442a5d
Georgeos Hardo <georgeoshardo@protonmail.com>
2024-01-18 04:08:05 +0000

    simulation stability

--- SyMBac/cell.py
+++ SyMBac/cell.py
@@ -24,118 +24,201 @@ class Cell:
         self.y_pos = y_pos
         self.angle = angle
         self.resolution = resolution
-        self.model = model
-        self.food_conc = food_conc
-        self.mass = mass
-        self.friction = friction
-        self.pm_object = self.initialise_pm_object()
-        self.division_threshold = division_threshold
-        self.mother = mother
+        self.angle = angle
+        self.position = position
+        self.space = space
+        self.max_length = max_length
+        self.max_length_mean = max_length_mean
+        self.max_length_var = max_length_var
+        self.body, self.shape = self.create_pm_cell()
+        self.angle = self.body.angle
+        self.ID = np.random.randint(0,100_000_000)
+        self.lysis_p = lysis_p
+        self.parent = parent
+        self.daughter = daughter
+        self.pinching_sep = pinching_sep
         
-        
-    
-    def get_cell_vertices_for_draw(self, cell_length, cell_width, resolution):
-    
-        def circ(theta, start, radius):
-            y = radius * np.cos(theta) +radius
-            x = radius * np.sin(theta) + start + radius
-            return x, y
 
-
-        def wall(radius, start, end, t_or_b):
-            wall_x = np.linspace(start, end, num = resolution)
-            wall_y = np.ones(resolution)*radius * t_or_b +radius
-            return wall_x, wall_y
-
-        cell_width = cell_width/2
-        cell_length = cell_length - cell_width
-        left_wall = circ(np.linspace(np.pi,2*np.pi, num=resolution), 0, cell_width)
-        top_wall_xy = wall(cell_width, cell_width, cell_length, 1)
-        bottom_wall_xy = wall(cell_width, cell_width, cell_length, -1)
-        right_wall = circ(np.linspace(0,np.pi, num=resolution), cell_length - cell_width, cell_width)
-        return [[left_wall[0][x] - cell_length/2, left_wall[1][x] - cell_width/2] for x in reversed(range(len(left_wall[0])))] + \
-                [[bottom_wall_xy[0][x] - cell_length/2, bottom_wall_xy[1][x]- cell_width/2] for x in (range(len(bottom_wall_xy[0])))] + \
-                [[right_wall[0][x] - cell_length/2, right_wall[1][x]- cell_width/2] for x in reversed(range(len(right_wall[0])))] + \
-                [[top_wall_xy[0][x] - cell_length/2, top_wall_xy[1][x]- cell_width/2] for x in reversed(range(len(top_wall_xy[0])))]
-
-    
-    def rotate(self, origin, point, angle):
+    def create_pm_cell(self):
         """
-        Rotate a point counterclockwise by a given angle around a given origin.
+        Creates a pymunk (pm) cell object, and places it into the pymunk space given when initialising the cell. If the
+        cell is dividing, then two cells will be created. Typically this function is called for every cell, in every
+        timestep to update the entire simulation.
 
-        The angle should be given in radians.
+        .. note::
+           The return type of this function is dependent on the value returned by :meth:`SyMBac.cell.Cell.is_dividing()`.
+           This is not good, and will be changed in a future version.
+
+        Returns
+        -------
+        dict or (pymunk.body, pymunk.shape)
+
+           If :meth:`SyMBac.cell.Cell.is_dividing()` returns `True`, then a dictionary of values for the daughter cell
+           is returned. A daughter can then be created. E.g:
+
+           >>> daughter_details = cell.create_pm_cell()
+           >>> daughter = Cell(**daughter_details)
+
+           If :meth:`SyMBac.cell.Cell.is_dividing()` returns `False`, then only a tuple containing (pymunk.body, pymunk.shape) will be returned.
         """
-        ox, oy = origin
-        px, py = point
 
-        qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-        qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-        return qx, qy
+        if self.is_dividing():
+            self.length = self.length/2 * 0.98 # It just has to be done in this order due to when we call self.body.position = [new_x, new_y]
+            daughter_length = self.length
+            self.pinching_sep = 0
+            
+            cell_vertices = self.calculate_vertex_list()
+            cell_shape = pymunk.Poly(None, cell_vertices)
+            self.shape = cell_shape
+            cell_mass = 0.000001
+            cell_moment = pymunk.moment_for_poly(cell_mass, cell_shape.get_vertices())
+            cell_body = pymunk.Body(cell_mass,cell_moment)
+            cell_shape.body  = cell_body
+            self.body = cell_body
+            new_x = self.position[0] + self.length/2 *  np.cos(self.angle)
+            new_y = self.position[1] + self.length/2 *  np.sin(self.angle)
+            self.body.position = [new_x, new_y]
+            cell_body.angle = self.angle
+            cell_shape.friction=0
+            #self.space.add(cell_body, cell_shape)
+            daughter_details = {
+                "length": daughter_length,
+                "width": np.random.normal(self.width_mean,self.width_var),
+                "resolution": self.resolution,
+                "position": [self.position[0] - self.length/2 *  np.cos(self.angle), self.position[1] - self.length/2 *  np.sin(self.angle)],
+                "angle": self.angle*np.random.uniform(0.95,1.05),
+                "space": self.space,
+                "dt": self.dt,
+                "growth_rate_constant": self.growth_rate_constant,
+                "max_length": np.random.normal(self.max_length_mean,self.max_length_var),
+                "max_length_mean": self.max_length_mean,
+                "max_length_var": self.max_length_var,
+                "width_var": self.width_var,
+                "width_mean": self.width_mean,
+                "lysis_p": self.lysis_p,
+                "parent": self.parent,
+                "pinching_sep": 0
+            }
+
+            
+            #self.position = [new_x, new_y]
+            return daughter_details
+        else:
+            cell_vertices = self.calculate_vertex_list()
+            cell_shape = pymunk.Poly(None, cell_vertices)
+            self.shape = cell_shape
+            cell_mass = 0.000001
+            cell_moment = pymunk.moment_for_poly(cell_mass, cell_shape.get_vertices())
+            cell_body = pymunk.Body(cell_mass,cell_moment)
+            cell_shape.body = cell_body
+            self.body = cell_body
+            cell_body.position = self.position
+            cell_body.angle = self.angle
+            cell_shape.friction=0
+            #self.space.add(cell_body, cell_shape)
+            return cell_body, cell_shape
+
+    def is_dividing(self): # This needs to be made constant or a cell can divide in one frame and not another frame
+        """
+        Checks whether a cell is dividing by comparing its current length to its max length (defined when instnatiated).
+
+        Returns
+        -------
+        output : bool
+            `True` if ``self.length > self.max_length``, else `False`.
+        """
+        if self.length > (self.max_length):
+            return True
+        else:
+            return False
 
 
-    def vertices(self):
-        points = np.array(self.get_cell_vertices_for_draw(self.length, self.width, self.resolution))
-        points[:,0] = points[:,0] + self.x_pos
-        points[:,1] = points[:,1] + self.y_pos
+    def update_length(self):
+        """
+        A method, typically called every timepoint to update the length of the cell according to ``self.length = self.length + self.growth_rate_constant*self.dt*self.length``.
 
-        rotated = np.zeros((len(points),2))
-        for x in range(len(points)):
-            rotated[x] = self.rotate((self.x_pos, self.y_pos), (points[x][0],points[x][1]), self.angle)
+        Contains additional logic to control the amount of cell pinching happening according to the difference between
+        the maximum length and the current length.
 
-        return rotated
+        Returns
+        -------
+        None
+        """
 
-    def growth_rate(self):
-        return self.food_conc
+        self.length = self.length + self.growth_rate_constant*self.dt*self.length*np.random.uniform(0.5,1.3)
+        self.pinching_sep = max(0, self.length - self.max_length + self.width)
+        self.pinching_sep = min(self.pinching_sep, self.width - 2)
 
+    def update_position(self):
+        """
+        A method, typically called every timepoint to keep synchronised the cell position (``self.position`` and ``self.angle``)
+        with the position of the cell's corresponding body in the pymunk space (``self.body.position`` and ``self.body.angle``).
 
-    def growth(self, dt):
-        if self.model == "adder":
-            self.length = self.length + dt * self.growth_rate()
-            self.is_dividing()
-            self.x_pos, self.y_pos = self.pm_object[1].position[0], self.pm_object[1].position[1]
-            self.angle = self.angle +  self.pm_object[1].angle
+        Returns
+        -------
+        None
+        """
+        self.position = self.body.position
+        self.angle = self.body.angle
+
+    def update_parent(self, parent):
+        """
+        Parameters
+        ----------
+        parent : :class:`SyMBac.cell.Cell`
+           The SyMBac cell object to assign as the parent to the current cell.
+
+        Returns
+        -------
+        None
+        """
+        self.parent = parent
+
+    def get_angle(self):
+        """
+        Gets the angle of the cell's pymunk body.
+
+        Returns
+        -------
+        angle : float
+           The cell's angle in radians.
+        """
+        return self.body.angle
     
-    def initialise_pm_object(self):
-        _poly_vertices = [tuple(vertex) for vertex in self.vertices().tolist()]
-        _poly = pm.Poly(None, _poly_vertices)
+    def calculate_vertex_list(self):
+        return cell_geometry.get_vertices(
+            self.length,
+            self.width,
+            0,#self.angle, 
+            self.resolution
+            )
 
-        _poly.friction = self.friction
-        _moment = pm.moment_for_poly(self.mass, _poly.get_vertices())
-        _body = pm.Body(self.mass, _moment)
-        _body._set_angle = self.angle
-        _poly.body = _body
-        
-        _body.position = self.x_pos, self.y_pos
-        
-        return (_poly, _body)
+    def get_vertex_list(self):
+        """
+        Calculates the vertex list (a set of x,y coordinates) which parameterise the outline of the cell
 
-    def update_pm_object(self):
-        self.pm_object = self.initialise_pm_object()
+        Returns
+        -------
+        vertices : list(tuple(float, float))
+           A list of vertices, each in a tuple, where the order is `(x, y)`. The coordinates are relative to the pymunk
+           space in which the cell exists.
+        """
+        vertices = []
+        for v in self.shape.get_vertices():
+            x,y = v.rotated(self.shape.body.angle) + self.shape.body.position #.rotated(self.shape.body.angle)
+            vertices.append((x,y))
+        return vertices
 
+    def get_centroid(self):
+        """
+        Calculates the centroid of the cell from the vertices.
 
-    def is_dividing(self):
-        if self.length > self.division_threshold * np.random.uniform(low = 0.9, high = 1.1):
-            return 1
-        if self.length < self.division_threshold:
-            return 0
-    def divide(self):
-        self.length = self.length/2
-        self.x_pos = self.x_pos - self.length/4 * np.cos(self.angle)
-        self.y_pos = self.y_pos - self.length/4 * np.sin(self.angle)
-        self.angle = np.random.uniform(low = self.angle - 2*np.pi*0.03, high = self.angle + 2*np.pi*0.03)
-    def centroid(self):
-        length = self.vertices().shape[0]
-        sum_x = np.sum(self.vertices()[:, 0])
-        sum_y = np.sum(self.vertices()[:, 1])
-        return sum_x/length, sum_y/length
+        Returns
+        -------
+        centroid : float
+            The cell's centroid in coordinates relative to the pymunk space which the cell exists in.
+        """
+        vertices = self.get_vertex_list()
+        return cell_geometry.centroid(vertices)
 
-    def daughter_length(self):
-        return self.length
-    def daughter_width(self):
-        return np.random.uniform(low = self.width*0.95, high = self.width*1.05)
-    def daughter_x_pos(self):
-        return self.x_pos + self.length/2 * np.cos(self.angle)
-    def daughter_y_pos(self):
-        return self.y_pos + self.length/2 * np.sin(self.angle)
-    def daughter_angle(self):
-        return np.random.uniform(low = self.angle - 2*np.pi*0.03, high = self.angle + 2*np.pi*0.03)
+    
--- SyMBac/cell_simulation.py
+++ SyMBac/cell_simulation.py
@@ -2,33 +2,246 @@
 import pyglet
 import pymunk as pm
 from pymunk.pyglet_util import DrawOptions
-import time
-import numpy as np
-from dataclasses import dataclass
-from typing import Any
-import math
-from joblib import Parallel, delayed
-from numba import njit
-from numba.experimental import jitclass
-from copy import deepcopy
-from cell import Cell
+import pymunk
+import pyglet
+from tqdm.auto import tqdm
+
+def run_simulation(trench_length, trench_width, cell_max_length, cell_width, sim_length, pix_mic_conv, gravity,
+                   phys_iters, max_length_var, width_var, save_dir, lysis_p=0, show_window = True, streamlit_mode = False):
+    """
+    Runs the rigid body simulation of bacterial growth based on a variety of parameters. Opens up a Pyglet window to
+    display the animation in real-time. If the simulation looks bad to your eye, restart the kernel and rerun the
+    simulation. There is currently a bug where if you try to rerun the simulation in the same kernel, it will be
+    extremely slow.
+
+    Parameters
+    ----------
+
+    trench_length : float
+        Length of a mother machine trench (micron)
+    trench_width : float
+        Width of a mother machine trench (micron)
+    cell_max_length : float
+        Maximum length a cell can reach before dividing (micron)
+    cell_width : float
+        the average cell width in the simulation (micron)
+    pix_mic_conv : float
+        The micron/pixel size of the image
+    gravity : float
+        Pressure forcing cells into the trench. Typically left at zero, but can be varied if cells start to fall into
+        each other or if the simulation behaves strangely.
+    phys_iters : int
+        Number of physics iterations per simulation frame. Increase to resolve collisions if cells are falling into one
+        another, but decrease if cells begin to repel one another too much (too high a value causes cells to bounce off
+        each other very hard). 20 is a good starting point
+    max_length_var : float
+        Variance of the maximum cell length
+    width_var : float
+        Variance of the maximum cell width
+    save_dir : str
+        Location to save simulation output
+    lysis_p : float
+        probability of cell lysis
+
+    Returns
+    -------
+    cell_timeseries : lists
+        A list of parameters for each cell, such as length, width, position, angle, etc. All used in the drawing of the
+        scene later
+    space : a pymunk space object
+        Contains the rigid body physics objects which are the cells.
+    """
+
+    space = create_space()
+    space.gravity = 0, gravity  # arbitrary units, negative is toward trench pole
+    #space.iterations = 1000
+    #space.damping = 0
+    #space.collision_bias = 0.0017970074436457143*10
+    space.collision_slop = 0.
+    dt = 1 / 20  # time-step per frame
+    pix_mic_conv = 1 / pix_mic_conv  # micron per pixel
+    scale_factor = pix_mic_conv * 3  # resolution scaling factor
+
+    trench_length = trench_length * scale_factor
+    trench_width = trench_width * scale_factor
+    trench_creator(trench_width, trench_length, (35, 0), space)  # Coordinates of bottom left corner of the trench
+
+    cell1 = Cell(
+        length=cell_max_length * scale_factor,
+        width=cell_width * scale_factor,
+        resolution=60,
+        position=(20 + 35, 10),
+        angle=0.8,
+        space=space,
+        dt= dt,
+        growth_rate_constant=1,
+        max_length=cell_max_length * scale_factor,
+        max_length_mean=cell_max_length * scale_factor,
+        max_length_var=max_length_var * np.sqrt(scale_factor),
+        width_var=width_var * np.sqrt(scale_factor),
+        width_mean=cell_width * scale_factor,
+        parent=None,
+        lysis_p=lysis_p
+    )
+
+    if show_window:
+
+        window = pyglet.window.Window(700, 700, "SyMBac", resizable=True)
+        options = DrawOptions()
+        options.shape_outline_color = (10,20,30,40)
+        @window.event
+        def on_draw():
+            window.clear()
+            space.debug_draw(options)
+
+        # key press event
+        @window.event
+        def on_key_press(symbol, modifier):
+
+            # key "E" get press
+            if symbol == pyglet.window.key.E:
+                # close the window
+                window.close()
+
+    #global cell_timeseries
+    #global x
+
+    #try:
+    #    del cell_timeseries
+    #except:
+    #    pass
+    #try:
+    #    del x
+    #except:
+    #    pass
+
+    x = [0]
+    cell_timeseries = []
+    cells = [cell1]
+    if show_window:
+        pyglet.clock.schedule_interval(step_and_update, interval=dt, cells=cells, space=space, phys_iters=phys_iters,
+                                       ylim=trench_length, cell_timeseries=cell_timeseries, x=x, sim_length=sim_length,
+                                       save_dir=save_dir)
+        pyglet.app.run()
+    else:
+        if streamlit_mode:
+            import streamlit as st
+            progress_text = "Simulation running"
+            my_bar = st.progress(0, text=progress_text)
+        for _ in tqdm(range(sim_length+2)):
+            step_and_update(
+                dt=dt, cells=cells, space=space, phys_iters=phys_iters, ylim=trench_length,
+                cell_timeseries=cell_timeseries, x=x, sim_length=sim_length, save_dir=save_dir
+            )
+            if streamlit_mode:
+                my_bar.progress((_)/sim_length, text=progress_text)
+
+    # window.close()
+    # phys_iters = phys_iters
+    # for x in tqdm(range(sim_length+250),desc="Simulation Progress"):
+    #    cells = step_and_update(dt=dt, cells=cells, space=space, phys_iters=phys_iters,ylim=trench_length*1.1, cell_timeseries = cell_timeseries, x=x, sim_length = sim_length, save_dir = save_dir)
+    #    if x > 250:
+    #        cell_timeseries.append(deepcopy(cells))
+    return cell_timeseries, space
+
+def create_space():
+    """
+    Creates a pymunk space
+
+    :return pymunk.Space space: A pymunk space
+    """
+
+    space = pymunk.Space(threaded=False)
+    #space.threads = 2
+    return space
+
+def update_cell_lengths(cells):
+    """
+    Iterates through all cells in the simulation and updates their length according to their growth law.
+
+    :param list(SyMBac.cell.Cell) cells: A list of all cells in the current timepoint of the simulation.
+    """
+    for cell in cells:
+        cell.update_length()
 
 
+def update_pm_cells(cells, space):
+    """
+    Iterates through all cells in the simulation and updates their pymunk body and shape objects. Contains logic to
+    check for cell division, and create daughters if necessary.
 
-cell_1 = Cell(length = 15, width = 7.5, y_pos = 768/4, x_pos = 1024/4, angle=2*np.pi*np.random.rand())
+    :param list(SyMBac.cell.Cell) cells: A list of all cells in the current timepoint of the simulation.
 
-space = pm.Space()
-options = DrawOptions()
-options.collision_point_color = (1,1,0,0)
-space.debug_draw(options)
-space.gravity = 0, 0
-cells =[cell_1]
-for cell in cells:
-    space.add(cell.pm_object[0], cell.pm_object[1])
+    """
+    for cell in cells:
+        cell.update_length()
+        if cell.is_dividing():
+            daughter_details = cell.create_pm_cell()
+            if len(daughter_details) > 2: # Really hacky. Needs fixing because sometimes this returns cell_body, cell shape. So this is a check to ensure that it's returing daughter_x, y and angle
+                daughter = Cell(**daughter_details)
+                cell.daughter = daughter
+                cells.append(daughter)
+        else:
+            cell.create_pm_cell()
+        cell_adder(cell, space)
+        for _ in range(150):
+            space.step(1/100)
 
-    
-    
-def space_updater(dt):
+def cell_adder(cell, space):
+    space.add(cell.body, cell.shape)
+
+
+def update_cell_positions(cells):
+    """
+    Iterates through all cells in the simulation and updates their positions, keeping the cell object's position
+    synchronised with its corresponding pymunk shape and body inside the pymunk space.
+
+    :param list(SyMBac.cell.Cell) cells: A list of all cells in the current timepoint of the simulation.
+    """
+    for cell in cells:
+        cell.update_position()
+
+def wipe_space(space):
+    """
+    Deletes all cells in the simulation pymunk space.
+
+    :param pymunk.Space space:
+    """
+    for body, poly in zip(space.bodies, space.shapes):
+        if body.body_type == 0:
+            space.remove(body)
+            space.remove(poly)
+
+def update_cell_parents(cells, new_cells):
+    """
+    Takes two lists of cells, one in the previous frame, and one in the frame after division, and updates the parents of
+    each cell
+
+    :param list(SyMBac.cell.Cell) cells:
+    :param list(SyMBac.cell.Cell) new_cells:
+    """
+    for i in range(len(cells)):
+        cells[i].update_parent(id(new_cells[i]))
+
+def step_and_update(dt, cells, space, phys_iters, ylim, cell_timeseries,x,sim_length,save_dir):
+    """
+    Evolves the simulation forward
+
+    :param float dt: The simulation timestep
+    :param list(SyMBac.cell.Cell)  cells: A list of all cells in the current timestep
+    :param pymunk.Space space: The simulations's pymunk space.
+    :param int phys_iters: The number of physics iteration in each timestep
+    :param int ylim: The y coordinate threshold beyond which to delete cells
+    :param list cell_timeseries: A list to store the cell's properties each time the simulation steps forward
+    :param int list: A list with a single value to store the simulation's progress.
+    :param int sim_length: The number of timesteps to run.
+    :param str save_dir: The directory to save the simulation information.
+
+    Returns
+    -------
+    cells : list(SyMBac.cell.Cell)
+
+    """
     for shape in space.shapes:
         space.remove(shape)
     for body in space.bodies:
@@ -52,37 +265,35 @@ def space_updater(dt):
                 space.step(dt)
         elif len(cells) > 100:
             space.step(dt)
+        else:
+            pass
+            #new_cells.append(cell)
+    #cells = deepcopy(new_cells)
+    #graveyard = deepcopy(graveyard)
 
-def get_draw_params():
-    for cell in cells:
-        print(cell.angle, cell.length, cell.width)
+    wipe_space(space)
 
+    update_pm_cells(cells, space)
 
-def cell_updater(cell):
-    cell.growth(np.random.uniform(low = 0, high = 0.2))
-    cell.update_pm_object()
-    if cell.is_dividing() == 1:
-        cell.divide()
-        cell.update_pm_object()
-        daughter = Cell(length = cell.daughter_properties.length(), width = cell.daughter_properties.width(), x_pos = cell.daughter_properties.x_pos(), y_pos = cell.daughter_properties.y_pos(), angle=cell.daughter_properties.angle())
-        cells.append(daughter)
+    for _ in range(phys_iters):
+        space.step(dt)
+    update_cell_positions(cells)
 
-def cell_adder(cell):
-    space.add(cell.pm_object[0], cell.pm_object[1])
+    #print(str(len(cells))+" cells")
+    if x[0] > 1:
+        #copy_cells = deepcopy(cells)
 
+        cell_timeseries.append(deepcopy(cells))
+        copy_cells = cell_timeseries[-1]
+        update_cell_parents(cells, copy_cells)
+        #del copy_cells
+    if x[0] == sim_length-1:
+        with open(save_dir+"/cell_timeseries.p", "wb") as f:
+            pickle.dump(cell_timeseries, f)
+        with open(save_dir+"/space_timeseries.p", "wb") as f:
+            pickle.dump(space, f)
+        pyglet.app.exit()
+        return cells
+    x[0] += 1
+    return (cells)
 
-def update(dt):
-    space_updater(dt)
-    get_draw_params()
-
-
-window = pyglet.window.Window(1024,768, "test", resizable = True)
-options = DrawOptions()
-@window.event
-def on_draw():
-    window.clear()
-    space.debug_draw(options)
-
-if __name__ == "__main__":
-    pyglet.clock.schedule_interval(update, 1/100)
-    pyglet.app.run()
